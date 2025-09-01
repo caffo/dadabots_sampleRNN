8450ba29649079b8b2da3a65e58f5f8fbfb0fcc1
Rodrigo Franco <caffo@users.noreply.github.com>
2025-09-01 16:46:52 -0300

    Update train32k.sh

--- train32k.sh
+++ train32k.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # train48k.sh
 # params: <cudaX> <dataset>
-# ./train32k.sh 0 dh_ocl
+# ./train32k.sh 0 k
 #
 # Since early epochs may cause errors, this script iterates and resume as needed.
 
-THEANO_FLAGS=mode=FAST_RUN,device=cuda$1,floatX=float32 python -u models/two_tier/two_tier32k.py --exp $2 --n_frames 64 --frame_size 16 --emb_size 256 --skip_conn True --dim 1024 --n_rnn 5 --rnn_type LSTM --q_levels 256 --q_type mu-law --batch_size 128 --weight_norm True --learn_h0 False --which_set $2 --resume;
+THEANO_FLAGS=mode=FAST_RUN,device=cuda$1,floatX=float32 python -u models/two_tier/two_tier32k.py --exp $2_experiment --n_frames 64 --frame_size 16 --emb_size 256 --skip_conn True --dim 1024 --n_rnn 5 --rnn_type LSTM --q_levels 256 --q_type mu-law --batch_size 128 --weight_norm True --learn_h0 False --which_set $2
 sleep 1;
 while true;
 do
-THEANO_FLAGS=mode=FAST_RUN,device=cuda$1,floatX=float32 python -u models/two_tier/two_tier32k.py --exp $2 --n_frames 64 --frame_size 16 --emb_size 256 --skip_conn True --dim 1024 --n_rnn 5 --rnn_type LSTM --q_levels 256 --q_type mu-law --batch_size 128 --weight_norm True --learn_h0 False --which_set $2 --resume; sleep 1; done;
-(p2) root@f3de9603ef64:/workspace/dadabots_sampleRNN#
+THEANO_FLAGS=mode=FAST_RUN,device=cuda$1,floatX=float32 python -u models/two_tier/two_tier32k.py --exp $2_experiment --n_frames 64 --frame_size 16 --emb_size 256 --skip_conn True --dim 1024 --n_rnn 5 --rnn_type LSTM --q_levels 256 --q_type mu-law --batch_size 128 --weight_norm True --learn_h0 False --which_set $2
